74d7833c8ef3399a7f6bb93bc20f0d7b8a958abe
Samyuktha Gopalsamy <samyuktha.gopalsamy@rutgers.edu>
2023-02-12 11:26:29 -0500

    Settings and Controller

--- src/main/java/com/example/zengage/HelloController.java
+++ src/main/java/com/example/zengage/HelloController.java
@@ -1,14 +1,26 @@
 package com.example.zengage;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private CheckBox appLimitToggle;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to ZenGage Application!");
+    private ComboBox<String> socialMediaAppDropdown;
+
+    @FXML
+    private ComboBox<String> addLimitDropdown;
+
+    @FXML
+    private ComboBox<String> preferencesDropdown;
+
+    @FXML
+    protected void initialize() {
+        socialMediaAppDropdown.getItems().addAll("Facebook", "Twitter", "Instagram", "Snapchat");
+        addLimitDropdown.getItems().addAll("30 minutes", "1 hour", "2 hours", "3 hours");
+        preferencesDropdown.getItems().addAll("Dark Mode", "Light Mode", "Custom");
     }
 }
